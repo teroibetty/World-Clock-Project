541abb1d42bc7f449715e32b552f91c87a1eb365
Bteroi <teroibetty@gmail.com>
2025-10-29 13:52:30 +0300

    displayed time and date of a city using moment.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisDateElement = parisElement.querySelector(".date");
 
+  parisDateElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("Do MMMM YYYY");
+  parisTimeElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+
+  newYorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("Do MMMM YYYY");
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
