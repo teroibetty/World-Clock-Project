8f781b77a273d4dcd2ef72914715479f5fc373e9
Bteroi <teroibetty@gmail.com>
2025-10-30 15:10:29 +0300

    Added the option to display the user's current location

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
