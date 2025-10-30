bde6451ca00b2acb973b89bc4cb2f427c3e9cae9
Bteroi <teroibetty@gmail.com>
2025-10-30 14:57:44 +0300

    Added functionality for the selected cities

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ function updateTime() {
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city" >
+            <div>
+              <h2>${cityTimeZone.split("/")[1]}</h2>
+              <div class="date">${cityTime.format("Do MMMM YYYY")}</div>
+            </div>
+            <div class="time">${cityTime.format(
+              "hh:mm:ss"
+            )}<small> ${cityTime.format("A")}</small></div>
+          </div>`;
+}
+
+let citySelectElement = document.querySelector("#select-city");
+citySelectElement.addEventListener("change", updateCity);
